205cb12935066278a90af6b26a18a62155117363
Rony Derra <derrarony@gmail.com>
2022-02-27 17:04:03 +0200

    five day card

--- src/components/FiveDayCard.js
+++ src/components/FiveDayCard.js
@@ -4,26 +4,71 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
+import ReactWeather from 'react-open-weather';
+
+
 
 export default function FiveDayCard(props) {
+
     console.log(props.data[0].Date)
+    console.log(props)
+
+    const data = {
+        forecast: [
+            {
+                date: props.data[1].Date,
+                description: 'Clear',
+                temperature: { min: '-0', max: '6' },
+                wind: '2',
+                humidity: 60,
+            },
+            {
+                date: props.data[4].Date,
+                description: 'Clouds',
+                temperature: { min: '-1', max: '6' },
+                wind: '3',
+                humidity: 67,
+            },
+            {
+                date: props.data[3].Date,
+                description: 'Clouds',
+                temperature: { min: '-1', max: '6' },
+                wind: '3',
+                humidity: 67,
+            },
+            {
+                date: props.data[2].Date,
+                description: 'Clouds',
+                temperature: { min: '-1', max: '6' },
+                wind: '3',
+                humidity: 67,
+            },
+            {
+                date: props.data[1].Date,
+                description: 'Clouds',
+                temperature: { min: '-1', max: '6' },
+                wind: '3',
+                humidity: 67,
+            },
+        ],
+        current: {
+            date: props.data[0].Date,
+            description: 'Clear',
+            temperature: { current: '-2', min: -3, max: 1 },
+            wind: '2',
+            humidity: 90,
+        },
+    };
+
     return (
         <div >
-            {props.data[0].Date}
-            {/* <Card>
-                <CardActions>
-                    <Button size="small">Add To Favorites</Button>
-                </CardActions>
-                <Typography gutterBottom variant="h5" component="h2"> Scatterd Clouds</Typography>
-                <CardContent sx={{ flexGrow: 1, flexDirection: 'row' }}>
-                    <Card sx={{ height: '100%' }}>
-                        sunday
-                    </Card>
-                    <Card sx={{ height: '100%', }}>
-                        monday
-                    </Card>
-                </CardContent>
-            </Card> */}
+            <ReactWeather
+                data={data}
+                lang="en"
+                locationLabel={props.local.LocalizedName}
+                unitsLabels={{ temperature: 'C', windSpeed: 'Km/h' }}
+                showForecast
+            />
         </div>
     );
 }
