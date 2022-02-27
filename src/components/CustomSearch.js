bd5e01d1cbc748df8bf82315010bf9d3ebdf163c
Rony Derra <derrarony@gmail.com>
2022-02-27 17:04:21 +0200

    custom search confi

--- src/components/CustomSearch.js
+++ src/components/CustomSearch.js
@@ -2,12 +2,19 @@ import * as React from 'react';
 import SearchField from "react-search-field";
 import axios from 'axios';
 import FiveDayCard from './FiveDayCard';
+import { margin } from '@mui/system';
 
 export default function CustomSearch() {
+
     const [searchData, setSearchData] = React.useState('');
+    const [KeyResponse, setKeyResponse] = React.useState('');
+
+
     const onSubmit = async (val) => {
+        
         const KeyResponse = await axios.get('http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f&q=' + val)
         const key = KeyResponse.data[0].Key
+        setKeyResponse(KeyResponse.data[0])
         const forecastReasponse = await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/` + key + `?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f`)
         const forecast = forecastReasponse.data;
         console.log(forecast.DailyForecasts)
@@ -15,15 +22,20 @@ export default function CustomSearch() {
     }
 
     return (
-        <div >
+        <div style={{margin:'auto'}}>
             <SearchField
                 placeholder="Search..."
                 searchText=""
                 classNames="test-class"
                 onEnter={(val) => onSubmit(val)}
                 onSearchClick={(val) => onSubmit(val)}
+                sx={{tp:'5px'}}
             />
-            <FiveDayCard data={searchData} />
+            <br />
+            <br />
+            <br />
+            { searchData ? <FiveDayCard data={searchData} local={KeyResponse} />  : <div></div>}
+           
         </div>
     );
 }
