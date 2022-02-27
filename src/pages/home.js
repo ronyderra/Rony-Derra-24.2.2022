09b778f15235f9e29ea633349d1be495b07ef25a
Rony Derra <derrarony@gmail.com>
2022-02-27 13:00:22 +0200

    home confi

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,8 +4,6 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -19,34 +17,23 @@ export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       {/* <CssBaseline /> */}
-      
- 
-
-
         <Container sx={{ py: 8 }} maxWidth="md">
         <CustomSearch />
-
           <Grid container spacing={4}>
-            
             {cards.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={12}>
                 <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-
                   <CardActions>
                     <Button size="small">Add To Favorites</Button>
                   </CardActions>
                   <Typography gutterBottom variant="h5" component="h2"> Scatterd Clouds</Typography>
-
                   <CardContent sx={{ flexGrow: 1, flexDirection: 'row' }}>
-
                     <Card sx={{ height: '100%' }}>
                       sunday
                     </Card>
                     <Card sx={{ height: '100%', }}>
                       monday
                     </Card>
-
-
                   </CardContent>
                 </Card>
               </Grid>
