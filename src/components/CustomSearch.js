c9c1e125d643485404f27597857b28b794bb66e0
Rony Derra <derrarony@gmail.com>
2022-02-27 02:02:21 +0200

    axios

--- src/components/CustomSearch.js
+++ src/components/CustomSearch.js
@@ -1,23 +1,27 @@
 import * as React from 'react';
 import SearchField from "react-search-field";
+import axios from 'axios';
+
 export default function CustomSearch() {
 
-    const [value, setValue] = React.useState('');
-    const [enterVal, setEnterVal] = React.useState('');
+    const onSubmit = async (val) => {
+        const KeyResponse = await axios.get('http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f&q=' + val)
+        const key = KeyResponse.data[0].Key
+        const forecastReasponse = await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/` + key + `?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f`)
+        const forecast = forecastReasponse.data;
+        console.log(forecast)
+        return (forecast)
+    }
 
     return (
         <div>
             <br />
-            <div>{`value: ${value !== null ? `'${value}'` : 'null'}`}</div>
-            <div>{`inputValue: '${enterVal}'`}</div>
-
             <SearchField
                 placeholder="Search..."
                 searchText=""
                 classNames="test-class"
-                onChange={(val)=>setValue(val)}
-                onEnter={(val)=>setEnterVal(val)}
-                onSearchClick={(val)=>setEnterVal(val)}
+                onEnter={(val) => onSubmit(val)}
+                onSearchClick={(val) => onSubmit(val)}
             />
         </div>
     );
