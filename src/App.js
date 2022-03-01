d093aa442741c9b09af4f57f62a92857919dcac0
Rony Derra <derrarony@gmail.com>
2022-03-02 01:34:53 +0200

    confi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
