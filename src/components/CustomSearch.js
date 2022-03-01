28824ba12e427b248cb7c41a1a9097d6fb677574
Rony Derra <derrarony@gmail.com>
2022-03-01 19:56:02 +0200

    new ui customSearch

--- src/components/CustomSearch.js
+++ src/components/CustomSearch.js
@@ -1,47 +1,54 @@
-import * as React from 'react';
-import SearchField from "react-search-field";
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import FiveDayCard from './FiveDayCard';
 import { Button } from '@mui/material';
 import { useDispatch } from 'react-redux'
 import { addFavorite } from '../Store/index'
+import { Autocomplete } from '@mui/material';
+import TextField from '@mui/material/TextField';
 
 const CustomSearch = () => {
-    const [searchData, setSearchData] = React.useState('');
-    const [KeyResponse, setKeyResponse] = React.useState('');
-    const [key, setKey] = React.useState('');
+    const [searchData, setSearchData] = useState('');
+    const [KeyResponse, setKeyResponse] = useState('');
+    const [key, setKey] = useState('');
+    const [value, setValue] = useState('');
+    const [inputValue, setInputValue] = useState('');
+    const [options, setOptions] = useState([{ LocalizedName: 'Santiago' }]);
+
     const dispatch = useDispatch()
 
-    const onSubmit = async (val) => {
-        const KeyResponse = await axios.get('http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f&q=' + val)
+    const onSubmit = async (chosenCity) => {
+        const KeyResponse = await axios.get('http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f&q=' + chosenCity)
         const key = KeyResponse.data[0].Key
         setKeyResponse(KeyResponse.data[0])
         const forecastReasponse = await axios.get(`http://dataservice.accuweather.com/forecasts/v1/daily/5day/` + key + `?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f&details=true&metric=true`)
         setKey(key)
-        const forecast = forecastReasponse.data;
-        setSearchData(forecast.DailyForecasts)
+        const forecast = forecastReasponse.data.DailyForecasts;
+        setSearchData(forecast)
     }
 
+    useEffect(async () => {
+        const res = await axios.get('http://dataservice.accuweather.com/locations/v1/topcities/150?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f')
+        const topOptions = res.data
+        setOptions(topOptions)
+        const resp = topOptions.findIndex(({ LocalizedName }) => LocalizedName === 'Tel Aviv')
+        setValue(resp)
+    }, []);
+
     return (
-        <div style={{ margin: 'auto' }}>
-            <SearchField
-                placeholder="Search..."
-                searchText=""
-                classNames="test-class"
-                onEnter={(val) => onSubmit(val)}
-                onSearchClick={(val) => onSubmit(val)}
-                sx={{ tp: '5px' }}
+        <div style={{ margin:'auto'}}>
+            <Autocomplete
+                id="controllable-states-demo"
+                value={value}
+                inputValue={inputValue}
+                onInputChange={(event, newInputValue) => {setInputValue(newInputValue); onSubmit(newInputValue)}}
+                options={options}
+                getOptionLabel={(option) => option.LocalizedName}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Choose City" />}
             />
             <Button onClick={() => dispatch(addFavorite(key))} >Add to favorites</Button>
-            <br />
-            <br />
-            <br />
-            {searchData ?
-                <FiveDayCard
-                    data={searchData}
-                    local={KeyResponse}
-                /> : <div></div>}
-
+            {searchData ? <FiveDayCard data={searchData} local={KeyResponse} /> : <div></div>}
         </div>
     );
 }
