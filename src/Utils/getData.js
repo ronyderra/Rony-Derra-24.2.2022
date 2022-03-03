a752038331415bc644538bf464979867175c52ee
Rony Derra <derrarony@gmail.com>
2022-03-03 20:00:12 +0200

    get data confi

--- src/Utils/getData.js
+++ src/Utils/getData.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_KEY = 'fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f'
+const API_KEY = 'apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f'
 const URL = 'https://dataservice.accuweather.com/'
 
 const autocomplete = async (chosenCity) => {
-  const autocompleteRes = await axios.get(URL +'locations/v1/cities/autocomplete?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f&q=' + chosenCity)
+  const autocompleteRes = await axios.get(URL + 'locations/v1/cities/autocomplete?' + API_KEY + '&q=' + chosenCity)
   return autocompleteRes.data
 }
 
@@ -14,18 +14,18 @@ const locations = async () => {
 }
 
 const forecasts = async (key) => {
-  const forecastsRes = await axios.get(URL +`forecasts/v1/daily/5day/` + key + `?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f&details=true&metric=true`)
+  const forecastsRes = await axios.get(URL + `forecasts/v1/daily/5day/` + key + `?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f&details=true&metric=true`)
   return forecastsRes.data
 }
 
 const currentConditions = async (key) => {
-  const conditionsRes = await axios.get(URL +'currentconditions/v1/' + key + '?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f&details=true')
+  const conditionsRes = await axios.get(URL + 'currentconditions/v1/' + key + '?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f&details=true')
   return conditionsRes.data
 }
 
 const cityName = async (key) => {
-  const cityNameRes = await axios.get(URL +'locations/v1/' + key + '?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f')
+  const cityNameRes = await axios.get(URL + 'locations/v1/' + key + '?apikey=fdyjyD2XskiXjlWqEtPAXkZ2KhdMSG8f')
   return cityNameRes.data
 }
 
-export default {cityName, currentConditions,  locations, autocomplete, forecasts, API_KEY, URL }
+export default { cityName, currentConditions, locations, autocomplete, forecasts, API_KEY, URL }
