1abb39f22e34510dc61452be6ab833dd6a0cebd3
Rony Derra <derrarony@gmail.com>
2022-03-20 16:09:42 +0200

    SearchInput

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import { Autocomplete, CircularProgress } from '@mui/material';
 import getData from '../Utils/getData'
+import FiveDayCard from './FiveDayCard';
 
 const SearchInput = () => {
     const [options, setOptions] = useState([]);
+    const [key, setKey] = useState("215854")
+    const [cityName, setCityName] = useState('Tel Aviv')
 
     const onChangeHandle = async (value) => {
         const response = await getData.autocomplete(value)
@@ -17,27 +20,25 @@ const SearchInput = () => {
         }
     };
 
-    const handleInputChang = (ev, newValue) => {
-        // console.log(ev)
-        // console.log(newValue)
-    }
-
 
     return (
-
-        <Autocomplete
-            id="cityAutocomplete"
-            style={{ width: '80%', margin: 'auto' }}
-            options={options}
-            getOptionLabel={(option) => option.cityName}
-            defaultValue={{ cityName: "Tel Aviv" }}
-            onInputChange={(event, newInputVal) => { onChangeHandle(newInputVal) }}
-
-
-            renderInput={(params) => { return <TextField {...params} label="Choose City" /> }
-            }
-        />
-
+        <>
+            <Autocomplete
+                id="cityAutocomplete"
+                style={{ width: '80%', margin: 'auto' }}
+                options={options}
+                getOptionLabel={(option) => option.cityName}
+                defaultValue={{ cityName: "Tel Aviv" }}
+               
+                onInputChange={(event, newInputVal) => { onChangeHandle(newInputVal) }}
+               
+                renderInput={(params) => { return <TextField {...params} label="Choose City" /> }}
+            />
+            <br />
+            <br />
+            <br />
+            <FiveDayCard keyVal={key} cityName={cityName} />
+        </>
     );
 }
 export default SearchInput;
