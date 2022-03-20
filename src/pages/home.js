5ec97155ea522ffdda3e9b1ee87a6ae5d6ee0fc5
Rony Derra <derrarony@gmail.com>
2022-03-20 16:49:35 +0200

    confi

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import CustomSearch from '../Components/CustomSearch'
 import SearchInput from '../Components/SearchInput'
 
 const Home = () => {
   return (
     <Container sx={{ py: 8 }} maxWidth="md">
       <SearchInput />
-      {/* <CustomSearch /> */}
     </Container>
   );
 }
